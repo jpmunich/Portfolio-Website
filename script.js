a7a2dd95a88a6bbafcc542c9af81e086c38b01fd
jpmunich <parkercoyle8@gmail.com>
2024-09-24 13:51:33 -0400

    Make scroll buttons/github logo switch functional

---
+++ script.js
@@ -0,0 +1,16 @@
+const githubLogo = document.getElementById("github-mark");
+githubLogo.addEventListener("mouseover", () => {
+  githubLogo.setAttribute("src", "./assets/GitHub_Logo_White.png");
+});
+
+githubLogo.addEventListener("mouseout", () => {
+  githubLogo.setAttribute("src", "./assets/github-mark-white.png");
+});
+
+document.getElementById("portfolio-button").addEventListener("click", () => {
+  document.getElementById("portfolio-title").scrollIntoView();
+});
+
+document.getElementById("about-button").addEventListener("click", () => {
+  document.getElementById("about-title").scrollIntoView();
+});
